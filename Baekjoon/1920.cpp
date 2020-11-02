083a5435e01aa886b042485e898c3bc2ad4c3944
Lee Minhoon <lmh_1220@naver.com>
2020-11-02 22:06:04 +0900

    init commit

---
+++ Baekjoon/1920.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(void)
+{
+    int n, m;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+
+    }
+    cin >> m;
+
+    return 0;
+}
