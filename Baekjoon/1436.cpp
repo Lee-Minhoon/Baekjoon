f2845ba96eddc8bf74f7fd554d387577032c6b67
Lee Minhoon <lmh_1220@naver.com>
2021-02-07 22:54:33 +0900

    init commit

--- Baekjoon/1436.cpp
+++ Baekjoon/1436.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
+#include<vector>
+#include<string>
 
 using namespace std;
 
 int main(void)
 {
+    int n; cin >> n;
+    vector<int> number;
+
+    int i = 666, cnt = 0;
+    while (cnt <= 10000) {
+        if (to_string(i).find("666") != string::npos) {
+            cnt++;
+            number.push_back(i);
+        }
+        i++;
+    }
+
+    cout << number[n - 1];
 
     return 0;
 }
-
-// ios_base::sync_with_stdio(false);
-// cin.tie(NULL);
-// cout.tie(NULL);
