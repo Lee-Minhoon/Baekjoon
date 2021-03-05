7aaeee3917385996d6e5009e4aed58ac46294956
Lee Minhoon <hackids@naver.com>
2021-03-06 03:43:17 +0900

    init commit

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -2,20 +2,55 @@
 
 using namespace std;
 
-void b(void)
+#define SIZE 21
+
+int S[SIZE][SIZE];
+int N;
+bool M[SIZE];
+int res = 1000000000;
+
+void synergy()
 {
-    cout << "that is true";
-    return;
+    int team1 = 0;
+    int team2 = 0;
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < N; j++) {
+            if (M[i] && M[i] == M[j]) team1 += S[i][j];
+            else if (!M[i] && M[i] == M[j]) team2 += S[i][j];
+        }
+    }
+    if (abs(team1 - team2) < res) {
+        res = abs(team1 - team2);
+    }
 }
 
-void a(void)
+void dfs(int cnt, int number)
 {
-    return;
+    if (cnt == N / 2) {
+        synergy();
+        return;
+    }
+    for (int i = number; i < N; i++) {
+        M[i] = true;
+        dfs(cnt + 1, i + 1);
+        M[i] = false;
+    }
 }
 
 int main(void)
 {
-    1 ? b() : NULL;
+    cin >> N;
+    N++;
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < N; j++) {
+            cin >> S[i][j];
+        }
+    }
+
+    dfs(0, 1);
+
+    cout << res;
 
     return 0;
 }
