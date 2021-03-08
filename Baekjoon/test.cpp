cf2917571adc36f1df131c9b64e12815d3b1226b
Lee Minhoon <hackids@naver.com>
2021-03-08 21:52:54 +0900

    init commit

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,22 +1,30 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
-int DP[100];
-
-int fibonacci(int n)
+void pick(int n, vector<int>& picked, int toPick)
 {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    if (DP[n] != 0) return DP[n];
-    return DP[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    if (toPick == 0) {
+        for (vector<int>::iterator it = picked.begin(); it < picked.end(); it++) {
+            cout << *it << " ";
+        }
+        cout << endl;
+    }
+    int smallest = picked.empty() ? 1 : picked.back() + 1;
+    for (int next = smallest; next <= n; ++next) {
+        picked.push_back(next);
+        pick(n, picked, toPick - 1);
+        picked.pop_back();
+    }
 }
 
 int main(void)
 {
-    int n; cin >> n;
+    int n, m; cin >> n >> m;
+    vector<int> nums;
 
-    cout << fibonacci(n);
+    pick(n, nums, m);
 
     return 0;
 }
