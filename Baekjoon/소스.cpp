948a0ad8688878508a6bc9a86c9d4038f8cdb58f
Lee Minhoon <hackids@naver.com>
2021-03-25 23:56:57 +0900

    init commit

---
+++ Baekjoon/소스.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+long long power(int A, int B)
+{
+	int res = 1;
+
+	for (int i = 0; i < B; i++) {
+		res *= A;
+	}
+
+	return res;
+}
+
+int main(void)
+{
+	int A = 2;
+	int B = 10;
+
+	cout << power(A, B);
+
+	return 0;
+}
