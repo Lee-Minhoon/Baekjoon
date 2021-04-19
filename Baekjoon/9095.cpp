f41ef04f8f3fa65a1d6cf23d67e123fc091d8f52
Lee Minhoon <hackids@naver.com>
2021-04-20 04:02:56 +0900

    init commit

--- Baekjoon/9095.cpp
+++ Baekjoon/9095.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int DP[12];
+int DP[11];
 
 int main(void)
 {
@@ -10,7 +10,7 @@ int main(void)
     DP[2] = 2;
     DP[3] = 4;
 
-    for (int i = 4; i < 12; i++) {
+    for (int i = 4; i < 11; i++) {
         DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3];
     }
 
