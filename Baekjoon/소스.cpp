5edf3ba76f5b930264a1c6a7caea7d6900cdfd27
Lee Minhoon <hackids@naver.com>
2021-05-14 23:56:32 +0900

    init commit

--- Baekjoon/소스.cpp
+++ Baekjoon/소스.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
+int center = 23;
+
 void star(int i, int j, int num)
 {
-	if (i / num % 3 == 1 && j / num % 3 == 1) {
-		cout << ' ';
-	}
+	// int center = (num * 2 - 1) / 2;
+	if (i / num == 0 && i % num == 0 && j == center) cout << '*';
+	else if ((i + 1) % num == 0) cout << '*';
+	else if (i == abs(center - j)) cout << '*';
 	else {
-		if (num == 1) {
-			cout << '*';
-		}
-		else {
-			star(i, j, num / 3);
-		}
+		// cout << ' ';
+		if (num == 3) cout << ' ';
+		else star(i, j, num / 2);
 	}
 }
 
@@ -22,8 +22,8 @@ int main(void)
 	int N; cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			star(i, j, N / 3);
+		for (int j = 0; j < N * 2 - 1; j++) {
+			star(i, j, N);
 		}
 		cout << '\n';
 	}
