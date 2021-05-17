ee2dbd3f048d52715faef0417df0ca89f9e9783c
Lee Minhoon <hackids@naver.com>
2021-05-18 06:32:02 +0900

    init commit

--- Baekjoon/10993.cpp
+++ Baekjoon/10993.cpp
@@ -7,16 +7,21 @@ int mid;
 
 void star(int y, int x, int num, int top, int bot)
 {
+    // 좌변과 우변의 위치
     int left = mid - abs(top - y);
     int right = mid + abs(top - y);
-    if (y == top && x == mid) cout << '*';
-    else if (y == bot && x >= left && x <= right) cout << '*';
+    // y좌표가 하단과 같고 x좌표가 좌변과 우변 범위안에 속해있다면 '*' 출력
+    if (y == bot && x >= left && x <= right) cout << '*';
     else if (x == left || x == right) {
-        if ((top > bot ? top : bot) > y && (top < bot ? top : bot) < y) cout << '*';
+        // x좌표가 좌변과 우변의 위치와 같고 y좌표가 상단과 하단 범위안에 속해있다면 '*' 출력
+        if ((top > bot ? top : bot) >= y && (top < bot ? top : bot) <= y) cout << '*';
+        // 아니라면 공백 출력
         else cout << ' ';
     }
     else {
+        // 어디에도 속하지 않는 부분은 공백 출력
         if (num == 1) cout << ' ';
+        // 상단과 하단 위치를 갱신하여 재귀 호출
         else star(y, x, num - 1, num % 2 == 0 ? bot + 1 : bot - 1, (top + bot) / 2);
     }
 }
@@ -30,7 +35,9 @@ int main(void)
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < (n % 2 == 0 ? width - i : width - (mid - i)); j++) {
+            // 제일 큰 삼각형의 형태가 역삼각형이라면
             if (n % 2 == 0) star(i, j, n, height - 1, 0);
+            // 아니라면
             else star(i, j, n, 0, height - 1);
         }
         cout << '\n';
--- Baekjoon/2448.cpp
+++ Baekjoon/2448.cpp
@@ -4,20 +4,29 @@ using namespace std;
 
 void star(int i, int j, int num, int mid)
 {
+	// 제일 상단 부분과 하단 부분
 	int top = i / num * num, bot = top + num - 1;
+	// 상단으로부터의 거리, 하단으로부터의 거리
 	int dis = abs(top - i), rdis = abs(bot - i);
+	// 왼쪽 끝과 오른쪽 끝 (상단 부분부터 늘어남)
 	int left = mid - dis, right = mid + dis;
+	// 왼쪽 끝과 오른쪽 끝 (상단 부분부터 줄어듦)
 	int rleft = mid - rdis, rright = mid + rdis;
 	if (num == 3) {
 		rleft++; rright--;
 	}
 
+	// 삼각형의 양변 밖에 있다면 공백
 	if (j < left || j > right) cout << ' ';
+	// 삼각형 안의 중앙 아래 삼각형
 	else if (dis >= num / 2 && j >= rleft && j <= rright) cout << ' ';
+	// 삼각형의 양변
 	else if (j == left || j == right) cout << '*';
 	else {
+		// 제일 작은 삼각형의 하단
 		if (num == 3) cout << '*';
 		else {
+			// 중앙값을 갱신하고 재귀 호출
 			if (dis >= num / 2 && j < mid) mid -= (num / 2);
 			else if (dis >= num / 2 && j > mid) mid += (num / 2);
 			star(i, j, num / 2, mid);
