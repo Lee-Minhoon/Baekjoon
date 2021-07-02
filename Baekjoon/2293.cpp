b7a84d94402bed3ba417c5184962b1d1e50dd9bf
Lee Minhoon <hackids@naver.com>
2021-07-03 01:18:14 +0900

    init commit

--- Baekjoon/2293.cpp
+++ Baekjoon/2293.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+const int MAX = 10001;
+
+int cache[MAX];
+
 int main(void)
 {
     int n, k; cin >> n >> k;
+
+    cache[0] = 1;
+    for (int i = 0; i < n; i++) {
+        int coin; cin >> coin;
+        for (int j = coin; j <= k; j++) {
+            cache[j] += cache[j - coin];
+        }
+    }
+
+    cout << cache[k];
 
     return 0;
 }
